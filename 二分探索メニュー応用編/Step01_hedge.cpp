6171f95d793e83e8d1d82c64c0668dac1f2e1333
Kaori Kobayashi(Takinami) <61788460+KaoriKoba@users.noreply.github.com>
2023-02-09 23:24:55 +0900

    Create Step01_hedge.cpp
    解の公式で解いてしまった

---
+++ 二分探索メニュー応用編/Step01_hedge.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+
+long long discriminant(long long a, long long b, long long c); // 判別式
+
+int main(void)
+{
+    long long N, K;
+    int maxT = 0;
+    
+    std::cin >> N >> K;
+    
+    for (int i = 0; i < N; ++i)
+    {
+        long long a, b, c;
+        long long d;
+        
+        std::cin >> a >> b >> c;
+        d = discriminant( a, b, c-K);
+        
+        if ( d >= 0 ) // 判別式が負の場合、木ははじめからKを越えている状態
+        {
+            double t;
+            t = (-b + sqrt(d)) / (2*a);
+            
+            if ( maxT < t )
+            {
+                maxT = std::ceil( t );
+            }
+        }
+        
+    }
+    
+    std::cout << maxT << std::endl;
+}
+
+// 判別式
+long long discriminant(long long a, long long b, long long c)
+{
+    long long d = (b*b)-(4*a*c);
+    return d;
+}
