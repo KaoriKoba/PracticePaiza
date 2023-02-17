11b8689e3e1f9c3ba687df30f8dbca826fa2dcd1
Kaori Kobayashi(Takinami) <61788460+KaoriKoba@users.noreply.github.com>
2023-02-17 17:24:21 +0000

    success!!!!

--- a_rank_level_up_problems/snake_map_step1.cs
+++ a_rank_level_up_problems/snake_map_step1.cs
@@ -8,35 +8,29 @@ class Program
         int[] data = new int[3];
         for (var i = 0; i < 3; i++)
         {
-            data[i] = int.Parse(line[0]);
+            data[i] = int.Parse(line[i]);
         }
 
-        char[,] map = new char[data[1], data[2]];
-        for (var y = 0; y < data[1]; y++) // y
+        // index 0 -> Hy, 1 -> Wx, 2 -> N
+        char[,] map = new char[data[0], data[1]];
+        for (var y = 0; y < data[0]; y++) // y
         {
-            String[] strings = Console.ReadLine().Trim().Split(' ');
+            String strings = Console.ReadLine().Trim();
 
-            for (var x = 0; x < data[2]; x++) // x
+            for (var x = 0; x < data[1]; x++) // x
             {
-                map[y,x] = char.Parse(strings[x]);
+                map[y,x] = strings[x];
             }
         }
 
-        // 表示する座標を取得する
-        for (var i = 0; )
-
-
-        /*
-        var line1 = Console.ReadLine().Trim();
-        var N = int.Parse(line1);
-        for (var y = 0; y < N; y++)
+        // 表示する座標を取得して表示
+        for (var i = 0; i < data[2]; i++ )
         {
-            string[] stArrayData = Console.ReadLine().Trim().Split(' ');
-            Console.WriteLine("hello = {0} , world = {1}", stArrayData[0], stArrayData[1]);
+            line = Console.ReadLine().Trim().Split(' ');
+            int y = int.Parse(line[0]);
+            int x = int.Parse(line[1]);
+            Console.WriteLine(map[y, x]);
         }
-        */
 
-        Console.WriteLine(data[0]);
-        Console.WriteLine("XXXXXX");
     }
 }
