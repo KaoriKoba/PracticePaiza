84f05a072a61a5d618fe6820d2a2208ecc897c50
Kaori Kobayashi(Takinami) <61788460+KaoriKoba@users.noreply.github.com>
2023-06-26 00:02:39 +0900

    Create a_rank_snake_map_step3.cpp
    https://paiza.jp/works/mondai/a_rank_level_up_problems/a_rank_snake_map_step3
    私の回答
    try catchからチェック用メンバを増やしました。

---
+++ CPP_a_rank_level_up_problems/a_rank_snake_map_step3.cpp
@@ -0,0 +1,123 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <vector>
+#include <exception>
+#include <stdexcept>
+
+#define MAP_NO_DATA '#'
+
+struct Point
+{
+	int x;
+	int y;
+};
+
+class Map
+{
+public:
+	Map() = default;
+	~Map() = default;
+
+private:
+	std::vector<std::string> lines;
+
+public:
+	void Add(const std::string& line) { lines.push_back(line); }
+	char Get(int x, int y) const;
+	char Get(const Point& p) const;
+	int Modify(int x, int y, char newdata);
+	int Modify(const Point& p, char newdata);
+	void Print();
+
+protected:
+	bool CheckRange(int x, int y) const;
+	bool CheckRange(const Point& p) const;
+};
+
+bool Map::CheckRange(int x, int y) const
+{
+	if (0 <= y && lines.size() > y)
+	{
+		if (0 <= x && lines[y].length() > x)
+		{
+			return true;
+		}
+	}
+	
+	return false;
+}
+
+bool Map::CheckRange(const Point& p) const
+{
+	return CheckRange(p.x, p.y);
+}
+
+char Map::Get(int x, int y) const
+{
+	if ( CheckRange(x, y) )
+	{
+		return lines[y][x];
+	}
+	
+	return MAP_NO_DATA;
+}
+
+char Map::Get(const Point& p) const
+{
+	return Get(p.x, p.y);
+}
+
+int Map::Modify(int x, int y, char newdata)
+{
+	if ( CheckRange(x, y) )
+	{
+		lines[y][x] = newdata;
+		return 0;
+	}
+
+	return	-1;
+}
+
+int Map::Modify(const Point& p, char newdata)
+{
+	return Modify(p.x, p.y, newdata);
+}
+
+void Map::Print()
+{
+	for (std::string l : lines)
+	{
+		std::cout << l << std::endl;
+	}
+}
+
+int main()
+{
+	int H, W;
+	Map map;
+
+	std::cin >> H >> W;
+
+	for ( int i = 0; i < H; i++ )
+	{
+		std::string line;
+
+		std::cin >> line;
+		map.Add(line);
+	}
+
+	for ( int i = 0; i < H; i++ )
+	{
+		for ( int j = 0; j < W; j++ )
+		{
+			if ( map.Get(j - 1, i) == '#' && map.Get(j + 1, i) == '#')
+			{
+				std::cout << i << ' ' << j << std::endl;
+			}
+		}
+
+	}
+
+	return 0;
+}
